f55f4b99339cfbe4698166fc5eaa3dfc583946fb
kason <me@kason.fun>
2025-02-04 21:35:46 +0800

    fix: release build

--- packages/deft-webpack-plugin/src/index.js
+++ packages/deft-webpack-plugin/src/index.js
@@ -1,6 +1,9 @@
 const child_process = require('child_process');
 
 class DeftWebpackPlugin {
+    constructor(options) {
+        this.options = options || {};
+    }
     apply(compiler) {
         const isServe = process.env.WEBPACK_SERVE == "true";
         if (isServe) {
@@ -10,14 +13,16 @@ class DeftWebpackPlugin {
                 process.exit(1);
             }
             const serverUrl = `http://localhost:${port}`
+            const cargoRunCmd = this.options.runCommand || "cargo run";
             compiler.hooks.emit.tapAsync('DeftWebpackPlugin', (compilation, callback) => {
-                let result = child_process.spawn('cargo', ['run'], {
+                let result = child_process.spawn(cargoRunCmd, {
                     env: {
                         ...process.env,
                         DEFT_JS_URL: serverUrl,
                     },
                     stdio: "inherit",
                     cwd: ".",
+                    shell: true,
                 });
                 result.on('exit', () => {
                     process.exit(0);
@@ -25,15 +30,25 @@ class DeftWebpackPlugin {
                 callback();
             });
         } else {
+            const outputDir = compiler.options.output?.path;
+            if (!outputDir) {
+                console.error("output.path is unspecified");
+                process.exit(1);
+            }
+            const cargoBuildCmd = this.options.buildCommand || "cargo build --release"
             compiler.hooks.afterEmit.tapAsync('DeftWebpackPlugin', (compilation, callback) => {
-                let result = child_process.spawn('cargo', ['build', '--release'], {
+                let result = child_process.spawn(cargoBuildCmd, {
                     env: {
                         ...process.env,
-                        DEFT_JS_URL: "dist",
+                        DEFT_JS_DIR: outputDir,
                     },
                     stdio: "inherit",
                     cwd: ".",
+                    shell: true,
                 });
+                result.on('error', () => {
+                    process.exit(1);
+                })
                 result.on('exit', () => {
                     callback();
                 });
